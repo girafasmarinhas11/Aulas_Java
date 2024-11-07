adc011a07c6a3cfb0ab8a99bd3169ff53773444b
Sebastian <sebastiangaonasilva@gmail.com>
2024-11-07 09:28:55 -0300

    07/11/2024 - exemplos aula06

---
+++ src/Aula06/Exemplos/Exemplo1.java
@@ -0,0 +1,28 @@
+package Aula06.Exemplos;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+        int[] numero = new int[5];
+
+        numero[0] = 10;
+        numero[1] = 20;
+        numero[2] = 30;
+        numero[3] = 40;
+        numero[4] = 50;
+        /*caso o valor da matriz não esteja declarado, será dado valor 0*/
+
+        System.out.println(numero[0]);
+
+        String[] nomes = new String[4];
+
+        nomes[0] = "Sebas";
+        nomes[1] = "Julia";
+        nomes[2] = "Ana";
+        nomes[3] = "Louisy";
+
+        System.out.println(nomes[3]);
+
+
+
+    }
+}
---
+++ src/Aula06/Exemplos/Exemplo3.java
@@ -0,0 +1,14 @@
+package Aula06.Exemplos;
+
+public class Exemplo3 {
+
+    public static void main(String[] args) {
+        int[] numeros = {1,2,3,4,5};
+
+        System.out.println("Valor da posição [4] = " + numeros[4]);
+
+        String[] nomes = {"Sebas","Julia", "Raphael", "Marcelo"};
+
+        System.out.println("Valor da posição [1] = " + nomes[3]);
+    }
+}
