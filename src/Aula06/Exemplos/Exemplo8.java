d3eff122536e60e2a6749acf13e3520813374466
Sebastian <Sebastiangaonasilva@gmail.com>
2024-11-08 09:25:16 -0300

    continuação dos exemplos - 08/11/2024

---
+++ src/Aula06/Exemplos/Exemplo8.java
@@ -0,0 +1,24 @@
+package Aula06.Exemplos;
+
+public class Exemplo8 {
+    public static void main(String[] args) {
+        int[][] matrizNumeros = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        for (int[] matrizNumero : matrizNumeros) {
+            for (int i : matrizNumero) {
+                System.out.println(matrizNumero[i]);
+            }
+        }
+
+        String[][] nomes = {{"Sebas","Toby"},{"Julia","Ana"}};
+
+        for(String[] nome: nomes){
+            for (String s : nome){
+                System.out.println(s);
+            }
+        }
+    }
+}
